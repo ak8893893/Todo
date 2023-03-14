83b4c7efd97c5274e45ae0a7a95aef4961494fcd
ak <ak8893893@yahoo.com.tw>
2023-03-14 12:18:08 +0800

    48-繼承抽象化驗證資料類別

--- Todo/Dto/TodoListPutDto.cs
+++ Todo/Dto/TodoListPutDto.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;   // 標籤驗證所需的函式庫
+using Todo.Abstracts;
 using Todo.Models;
 using Todo.ValidationAttributes;
 
 namespace Todo.Dto
 {
-    public class TodoListPutDto
+    public class TodoListPutDto : TodoListEditDtoAbstract
     {
         public Guid TodoId { get; set; }
-        [TodoName]
-        public string Name { get; set; }
-        public bool Enable { get; set; }
-        [Range(0,100)]
-        public int Orders { get; set; }
-        public ICollection<UploadFilePostDto> UploadFiles { get; set; }
-
     }
 }
